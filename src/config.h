97367fc675bef1e9b511c4ca5bb41f972acc5141
HanibalEckzahn <j.habermann14@gmail.com>
2022-01-01 16:57:05 +0100

    first commit

---
+++ src/config.h
@@ -0,0 +1,12 @@
+
+//WIFI Settings
+
+#define HOSTNAME "Roomba"
+#define SSID "Your SSID HERE"
+
+//MQTT Settings
+
+#define MQTT_Server "192.168.x.x"
+#define MQTT_PORT "1888"
+#define MQTT_User "xxxx"
+#define MQTT_Password "password"
