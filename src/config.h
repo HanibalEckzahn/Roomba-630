df473743800c9f8dfe90f5e3aec2e89e59e00181
HanibalEckzahn <j.habermann14@gmail.com>
2022-01-01 17:58:45 +0100

    test

--- src/config.h
+++ src/config.h
@@ -2,7 +2,14 @@
 //WIFI Settings
 
 #define HOSTNAME "Roomba"
-#define SSID "Your SSID HERE"
+// #define SSID "Your SSID HERE"
+#define PASSWORD "string"
+
+// WIFI AP 
+const char *APssid = "Roombot";
+const char *APpassword = "momo2021";
+
+
 
 //MQTT Settings
 
@@ -10,3 +17,8 @@
 #define MQTT_PORT "1888"
 #define MQTT_User "xxxx"
 #define MQTT_Password "password"
+
+//ESP Settings
+#define SERIAL_RX     D5  // pin for SoftwareSerial RX
+#define SERIAL_TX     D6  // pin for SoftwareSerial TX
+#define Wake_Pin      D1
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include <Arduino.h>
+#include <config.h>
 #include <Roomba.h>
+#include <TimeLib.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(SERIAL_RX, SERIAL_TX); // (RX, TX. inverted, buffer)
+
+
 
 void setup() {
+
+  Serial.begin(115200);
+  mySerial.begin(115200);
+  pinMode(SERIAL_RX, INPUT);
+  pinMode(SERIAL_TX, OUTPUT);
+  pinMode(Wake_Pin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(Wake_Pin,LOW);
+  WiFi.begin("Alice im WLAN",PASSWORD);
+  //int i = 0;
   // put your setup code here, to run once:
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
+
