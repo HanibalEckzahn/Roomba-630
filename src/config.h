3ba68a74303c90d5ddcead5c2bc302dea3d46e83
HanibalEckzahn <j.habermann14@gmail.com>
2022-01-02 14:08:24 +0100

    add html

--- src/config.h
+++ src/config.h
@@ -8,6 +8,15 @@ String WMode = "1";
 // #define SSID "Your SSID HERE"
 #define PASSWORD "string"
 
+EspMQTTClient client(
+  "SSID",
+  "PASSWORD",
+  "192.168.1.100",
+  "MQTTUsername",
+  "MQTTPassword",
+  "TestClient1"
+);
+
 // WIFI AP 
 const char *APssid = "Roombot";
 const char *APpassword = "momo2021";
@@ -20,6 +29,19 @@ const char *APpassword = "momo2021";
 #define MQTT_PORT "1888"
 #define MQTT_User "xxxx"
 #define MQTT_Password "password"
+
+// Pimatic settings
+String host   = "192.168.1.25";
+const int httpPort    = 1099;
+String Username     = "iot";
+String Password     = "broker";
+String chargevar = "chargestatus";
+String distancevar = "distance";
+char authVal[40];
+char authValEncoded[40];
+
+
+
 
 //ESP Settings
 #define SERIAL_RX     D5  // pin for SoftwareSerial RX
--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,18 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <ArduinoOTA.h>
-#include <Roomba.h>
-#include <config.h>
-#include <Roomba.h>
-#include <TimeLib.h>
-#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <SoftwareSerial.h>
 #include "EspMQTTClient.h"
 #include <PubSubClient.h> 
+#include <Roomba.h>
+#include <config.h>
+#include <TimeLib.h>
 
 SoftwareSerial mySerial(SERIAL_RX, SERIAL_TX); // (RX, TX. inverted, buffer)
 
 
+String ClientIP;
 
 // Div
 File UploadFile;
@@ -25,22 +24,12 @@ int FSUsed;
 
 
 // webserver
-ESP8266WebServer  server(80);
+ESP8266WebServer server(80);
 MDNSResponder   mdns;
 WiFiClient client;
 
 
-// Roomba setup
-Roomba roomba(&Serial, Roomba::Baud115200);
 
-EspMQTTClient client1(
-  SSID,
-  PASSWORD",
-  "192.168.1.100",
-  "MQTTUsername",
-  "MQTTPassword",
-  "TestClient1"
-);
 
 void setup() {
 
@@ -52,6 +41,14 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(Wake_Pin,LOW);
   WiFi.begin("Alice im WLAN",PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" connected");
+
+  server.begin();
   //int i = 0;
   // put your setup code here, to run once:
 }
@@ -64,22 +61,49 @@ void loop() {
   // put your main code here, to run repeatedly:
 }
 
+//HTML
+String header       =  "<html lang='de'><head><title>Roombot control panel</title><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'><script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script><script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'></script></head><body>";
+String navbar       =  "<nav class='navbar navbar-default'><div class='container-fluid'><div class='navbar-header'><a class='navbar-brand' href='/'>Roombot control panel</a></div><div><ul class='nav navbar-nav'><li><a href='/'><span class='glyphicon glyphicon-info-sign'></span> Status</a></li><li class='dropdown'><a class='dropdown-toggle' data-toggle='dropdown' href='#'><span class='glyphicon glyphicon-cog'></span> Tools<span class='caret'></span></a><ul class='dropdown-menu'><li><a href='/updatefwm'><span class='glyphicon glyphicon-upload'></span> Firmware</a></li><li><a href='/filemanager.html'><span class='glyphicon glyphicon-file'></span> File manager</a></li><li><a href='/fupload'> File upload</a></li></ul></li><li><a href='https://github.com/incmve/roomba-eps8266/wiki' target='_blank'><span class='glyphicon glyphicon-question-sign'></span> Help</a></li></ul></div></div></nav>";
+String containerStart   =  "<div class='container'><div class='row'>";
+String containerEnd     =  "<div class='clearfix visible-lg'></div></div></div>";
+String siteEnd        =  "</body></html>";
+
+String panelHeaderName    =  "<div class='col-md-4'><div class='page-header'><h1>";
+String panelHeaderEnd   =  "</h1></div>";
+String panelEnd       =  "</div>";
+
+String panelBodySymbol    =  "<div class='panel panel-default'><div class='panel-body'><span class='glyphicon glyphicon-";
+String panelBodyName    =  "'></span> ";
+String panelBodyValue   =  "<span class='pull-right'>";
+String panelcenter   =  "<div class='row'><div class='span6' style='text-align:center'>";
+String panelBodyEnd     =  "</span></div></div>";
+
+String inputBodyStart   =  "<form action='' method='POST'><div class='panel panel-default'><div class='panel-body'>";
+String inputBodyName    =  "<div class='form-group'><div class='input-group'><span class='input-group-addon' id='basic-addon1'>";
+String inputBodyPOST    =  "</span><input type='text' name='";
+String inputBodyClose   =  "' class='form-control' aria-describedby='basic-addon1'></div></div>";
+String roombacontrol     =  "<a href='/roombastart'<button type='button' class='btn btn-default'><span class='glyphicon glyphicon-play' aria-hidden='true'></span> Start</button></a><a href='/roombamax'<button type='button' class='btn btn-default'><span class='glyphicon glyphicon-play' aria-hidden='true'></span> Max Clean</button></a><a href='/roombastop'<button type='button' class='btn btn-default'><span class='glyphicon glyphicon-stop' aria-hidden='true'></span> Stop</button></a><a href='/roombaspot'<button type='button' class='btn btn-default'><span class='glyphicon glyphicon-cleaning' aria-hidden='true'></span> Spot</button></a><a href='/roombadock'<button type='button' class='btn btn-default'><span class='glyphicon glyphicon-home' aria-hidden='true'></span> Dock</button></a></div>";
 
 
-  server.on ( "/format", handleFormat );
-  server.on("/", handle_root);
-  server.on("/", handle_fupload_html);
-  server.on("/api", handle_api);
-  server.on("/updatefwm", handle_updatefwm_html);
-  server.on("/fupload", handle_fupload_html);
-  server.on("/filemanager_ajax", handle_filemanager_ajax);
-  server.on("/delete", handleFileDelete);
-  server.on("/roombastart", handle_roomba_start);
-  server.on("/roombamax", handle_roomba_max);
-  server.on("/roombastop", handle_roomba_stop);
-  server.on("/roombaspot", handle_roomba_spot);
-  server.on("/roombadock", handle_roomba_dock);
-  server.on("/restart", handle_esp_restart);
+
+
+//API 
+//TODO API completieren
+
+  //server.on ( "/format", handleFormat );
+  //server.on("/", handle_root);
+  //server.on("/", handle_fupload_html);
+  //server.on("/api", handle_api);
+  //server.on("/updatefwm", handle_updatefwm_html);
+  //server.on("/fupload", handle_fupload_html);
+  //server.on("/filemanager_ajax", handle_filemanager_ajax);
+  //server.on("/delete", handleFileDelete);
+  //server.on("/roombastart", handle_roomba_start);
+  //server.on("/roombamax", handle_roomba_max);
+  //server.on("/roombastop", handle_roomba_stop);
+  //server.on("/roombaspot", handle_roomba_spot);
+  //server.on("/roombadock", handle_roomba_dock);
+  //server.on("/restart", handle_esp_restart);
 
 
 
@@ -94,7 +118,7 @@ void handle_root()
 
   String title1     = panelHeaderName + String("Roombot Settings") + panelHeaderEnd;
   String IPAddClient    = panelBodySymbol + String("globe") + panelBodyName + String("IP Address") + panelBodyValue + ClientIP + panelBodyEnd;
-  String ClientName   = panelBodySymbol + String("tag") + panelBodyName + String("Client Name") + panelBodyValue + espName + panelBodyEnd;
+  String ClientName   = panelBodySymbol + String("tag") + panelBodyName + String("Client Name") + panelBodyValue + panelBodyEnd;
   String Version     = panelBodySymbol + String("info-sign") + panelBodyName + String("Roombot Version") + panelBodyValue + roombotVersion + panelBodyEnd;
   String Uptime     = panelBodySymbol + String("time") + panelBodyName + String("Uptime") + panelBodyValue + hour() + String(" h ") + minute() + String(" min ") + second() + String(" sec") + panelBodyEnd + panelEnd;
 
